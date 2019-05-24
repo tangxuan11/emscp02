f61b3ec5910827d8cdb485d7963be01b810c310d
tangxuan11 <50381007+tangxuan11@users.noreply.github.com>
2019-05-24 12:36:57 -0500

    Add mat theme to login and top-nav-bar-login

--- customer-portal/src/app/AppFeatures/Login/login.module.ts
+++ customer-portal/src/app/AppFeatures/Login/login.module.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 import { LoginComponent } from './login.component';
 
@@ -12,7 +15,10 @@ import { LoginComponent } from './login.component';
         CommonModule,
         FlexLayoutModule,
         MatIconModule,
-        MatButtonModule
+        MatButtonModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatToolbarModule
     ],
     exports: [
         LoginComponent
