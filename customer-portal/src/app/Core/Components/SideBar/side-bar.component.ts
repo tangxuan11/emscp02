6f7f6d0200ae93c3bbe1b5217c47911a8670924a
Michal Cudzich <Michal.Cudzich@infinite.com>
2019-05-30 17:32:29 -0500

    Modified themes. Updated Feature view to follow new design.

--- customer-portal/src/app/Core/Components/SideBar/side-bar.component.ts
+++ customer-portal/src/app/Core/Components/SideBar/side-bar.component.ts
@@ -1,11 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { SidebarNavLink } from './side-bar-link.component';
-
-interface SidebarNavGroup {
-    heading: string;
-    panels: SidebarNavLink[];
-}
+import { SidebarNavGroup, navLinks } from './nav-links';
 
 @Component({
     selector: 'app-side-bar',
@@ -14,97 +9,7 @@ interface SidebarNavGroup {
 })
 export class SideBarComponent implements OnInit {
     activeMainPanel: string;
-    private sideNav: SidebarNavGroup[] = [
-        {
-            heading: "Navigation",
-            panels: [
-                { title: "Dashboard",
-                  icon: "home",
-                  iconStyle: "outlined",
-                  route: "/dashboard"
-                },
-                { title: "Analytics",
-                  icon: "trending_up",
-                  route: "/analytics",
-                  subPanels: [
-                      {
-                          title: "tmp2",
-                          icon: "home",
-                          route: "/analytics/tmp2",
-                          isSubpanel: true
-                      },
-                      {
-                          title: "tmp3",
-                          icon: "home",
-                          route: "/analytics/tmp3",
-                          isSubpanel: true
-                      },
-                      {
-                          title: "tmp4",
-                          icon: "home",
-                          route: "/analytics/tmp4",
-                          isSubpanel: true
-                      },
-                      {
-                          title: "tmp5",
-                          icon: "home",
-                          route: "/analytics/tmp5",
-                          isSubpanel: true
-                      },
-                      {
-                          title: "tmp1",
-                          icon: "home",
-                          route: "/analytics/tmp1",
-                          isSubpanel: true
-                      }
-                  ]
-                },
-                { title: "Message Log",
-                  icon: "list",
-                  route: "/message_log"
-                },
-                { title: "Web Text",
-                  icon: "chat",
-                  iconStyle: "outlined",
-                  route: "/web_text"
-                },
-                { title: "Registries",
-                  icon: "group",
-                  iconStyle: "outlined",
-                  route: "/registries"
-                },
-                { title: "Campaign",
-                  icon: "send",
-                  iconStyle: "outlined",
-                  route: "/campaign"
-                },
-                { title: "CMS",
-                  icon: "donut_small",
-                  iconStyle: "outlined",
-                  route: "/cms"
-                },
-                { title: "Enterprise",
-                  icon: "domain"
-                }
-            ]
-        },
-        {
-            heading: "Account",
-            panels: [
-                { title: "Notifications",
-                  icon: "notifications",
-                  iconStyle: "outlined",
-                },
-                { title: "Settings",
-                  icon: "settings",
-                  iconStyle: "outlined",
-                },
-                { title: "Help",
-                  icon: "help_outline"
-                }
-            ]
-        }
-    ];
+    private sideNav: SidebarNavGroup[] = navLinks;
 
     constructor() { }
 
