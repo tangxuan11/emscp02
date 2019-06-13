b8037fe68f02ac6e62ddc566145cd3117afc1d85
tangxuan11 <50381007+tangxuan11@users.noreply.github.com>
2019-06-13 14:06:14 -0500

    0613a

--- customer-portal/src/app/AppFeatures/Dashboard/dashboard.component.ts
+++ customer-portal/src/app/AppFeatures/Dashboard/dashboard.component.ts
@@ -10,32 +10,32 @@ export class DashboardComponent implements OnInit {
     featureTitle1: string = "Analytics";
     featureURL1: string = "/analytics";
     featureDescription1: string = "Analyze Traffic Performance";
-    featureLogoFileName1: string = "Logo1";
+    featureLogoFileName1: string = "assets/icons/analytics_thumbnail.png";
 
     featureTitle2: string = "Message Log";
     featureURL2: string = "/message_log";
     featureDescription2: string = "View Message History";
-    featureLogoFileName2: string = "Logo2";
+    featureLogoFileName2: string = "assets/icons/message log_thumbnail.png";
 
     featureTitle3: string = "Web Text";
     featureURL3: string = "/web_text";
     featureDescription3: string = "Send a message from your browser";
-    featureLogoFileName3: string = "Logo3";
+    featureLogoFileName3: string = "assets/icons/webtext_thumbnail.png";
 
     featureTitle4: string = "Registries";
     featureURL4: string = "/registries";
     featureDescription4: string = "Manage Opt-in & Opt-out registries";
-    featureLogoFileName4: string = "Logo4";
+    featureLogoFileName4: string = "assets/icons/registries_thumbnail.png";
 
     featureTitle5: string = "Campaign";
     featureURL5: string = "/campaign";
     featureDescription5: string = "Manage & launch campaigns";
-    featureLogoFileName5: string = "Logo5";
+    featureLogoFileName5: string = "assets/icons/campaign_thumbnail.png";
 
     featureTitle6: string = "CMS";
     featureURL6: string = "/cms";
     featureDescription6: string = "Customized workflows via templates using Content Management System";
-    featureLogoFileName6: string = "Logo6";
+    featureLogoFileName6: string = "assets/icons/cms_thumbnail.png";
 
     constructor() { }
 
